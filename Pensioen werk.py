cb06e2ee756238c56a10c965082d5d4f79f52a35
mcvha6 <marco.viergever@gmail.com>
2023-06-20 19:37:59 +0200

    bijwerken en afronden van het programma

--- Pensioen werk.py
+++ Pensioen werk.py
@@ -1,21 +1,24 @@
 def pensioen(status,leeftijd): # berekent je pensioen
     # Check leeftijd
     if leeftijd < 65:
-        uitvoer = f"U mag nog {65-leeftijd} jaar werken tot uw pensioen"
+        uitvoer = f"U mag nog {65-leeftijd} jaar werken tot uw pensioen."
         return uitvoer
     else:
-        if leeftijd > 69:   # als je  ouder bent dan 70 krig je meer pensien en daarvoor moet je verder in de list.
-            lf=3        
+        if leeftijd > 69:   # als je  ouder bent dan 70 krig je meer pensioen en daarvoor moet je verder in de list.
+            lf=3        # hoeveel verder je in de lijst moet wezen als 70 plusser
         else:
             lf=0
-    # check de werkstatuut
-    if status == "z":
-        uitvoer = f"U krijgt voor € {uitkering[0+lf]} per week aan pensioen"
+    # check de werkstatuut door naar de 1e letter te kijken van de invoer
+    if status[0] == "a":
+        uitvoer = f"Als ambtenaar krijgt u voor € {uitkering[2+lf]} per week aan pensioen."
     else:
-        if status == "m":
-            uitvoer = f"U krijgt voor € {uitkering[1+lf]} per week aan pensioen"
+        if status[0] == "m":
+            uitvoer = f"Als medewerker krijgt u voor € {uitkering[1+lf]} per week aan pensioen."
         else:
-            uitvoer = f"U krijgt voor € {uitkering[2+lf]} per week aan pensioen"
+            if status[0] == "z":
+                uitvoer = f"Als zelfstandige krijgt u voor € {uitkering[0+lf]} per week aan pensioen."
+            else:
+                uitvoer = "Sorry u krijgt geen pensioen of u heeft iets verkeerds ingevuld."
     return uitvoer
 
 uitkering=[300,350,370,315,370,395]   # de uitkeringsbedragen in de volgorde zelfstandig, medewerker, ambtenaar, en daarachter als ze 70+ zijn
@@ -26,4 +29,3 @@ werkstatuut=(input("Voer in: (m)edewerker, (z)elfstandige of (a)mbtenaar: "))
 print()
 print()
 print(pensioen(werkstatuut,leeftijd))
-
